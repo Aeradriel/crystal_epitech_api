12c4a25d856327aa62ea7d5dd8d5bee97ba8b025
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-09-12 01:10:04 +0200

    [C] Doc for EpitechApi module

--- src/epitech_api.cr
+++ src/epitech_api.cr
@@ -3,15 +3,24 @@ require "json"
 require "./epitech_api/*"
 
 module EpitechApi
+  # Base url for all requests
   BASE_URL = "https://intra.epitech.eu"
+  # Path to sign in
   SIGN_IN_PATH = { method: "post", path: "/login" }
 
   @@access_token : String?
 
+  # Getter for the access token
   def self.access_token
     @@access_token
   end
 
+  # Sign the user in with the credentials passed as arguments.
+  # If the sign in is a success, @@access_token is populated.
+  #
+  #```
+  # EpitechApi.sign_in("roche_t", "fakepass", "on") # : String
+  #```
   def self.sign_in(login : String, password : String , remember_me : String = "on")
     response = EpitechApi.request(SIGN_IN_PATH, login: login, password: password, remember_me: remember_me)
     json = JSON.parse(response.body)
@@ -22,6 +31,12 @@ module EpitechApi
     end
   end
 
+  # Send a request depending on the given route.
+  # You can pass arguments that will be submitted in POST requests as named tuple.
+  #
+  #```
+  # EpitechApi.request(SIGN_IN_PATH, login: login, password: password, remember_me: remember_me) # : HTTP::Client::Response
+  #```
   def self.request(route : NamedTuple(method: String, path: String), **args)
     url = BASE_URL + route[:path].gsub("//", "/") + "?format=json"
     url += "&access_token=#{@@access_token}" if @@access_token
@@ -33,6 +48,11 @@ module EpitechApi
     end
   end
 
+  # Converts a named tuple into a string ready for being used in an HTTP::Request.
+  #
+  # ```
+  # EpitechApi.named_tuple_to_args({name: "test"}) # => "?name=test"
+  # ```
   def self.named_tuple_to_args(tuple : NamedTuple)
     args = ""
     tuple.each do |k, v|
